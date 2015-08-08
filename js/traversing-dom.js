96019422e38b3a3db5cdb0ced88328353f7fe493
MartinChavez <martin.chavez@live.com>
2015-08-08 13:10:13 -0700

    Traversing the DOM

--- js/traversing-dom.js
+++ js/traversing-dom.js
@@ -23,4 +23,26 @@ $(document).ready(function () {
     $("#js-frameworks li:last").text("jQuery: Last element");
     // Index starts at 0
     $("#js-frameworks li:odd").text("jQuery: Odd elements");
+
+
+    /* Traversing */
+    // It is faster to find elements in the DOM using this method
+
+    // The first part is called 'selection'
+    // The second part is called 'traversal'
+    $("#js-frameworks-traversing").find("li").text("jQuery: traversing");
+
+    /* Filtering by traversing */
+    // You can use jQuery methods to achieve this
+    $("#js-frameworks-traversing").find("li").first().text("First");
+    $("#js-frameworks-traversing").find("li").last().text("Last");
+
+    /* Walking the DOM */
+    // You can use method chaining the manipulate specific DOM elements
+    // These methods are: next(),prev(),parent() and children()
+    $("#js-frameworks-traversing").find("li").first().next().text("Second");
+    $("#js-frameworks-traversing").find("li").first().next().prev().text("First");
+    $("#js-frameworks-traversing").find("li").parent();
+    // Children() is more specific than Find() since it only finds the direct children nodes
+    $("#js-frameworks-traversing").children("li");
 });
