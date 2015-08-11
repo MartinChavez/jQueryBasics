0637808bcb3cf156eab70a3298e1183480dda8d4
MartinChavez <martin.chavez@live.com>
2015-08-10 20:58:35 -0700

    DOM Events

--- js/dom-events.js
+++ js/dom-events.js
@@ -2,5 +2,13 @@ $(document).ready(function () {
 
     /* DOM Events */
 
+    // slideDown() - will show an element with an effect
+    // slideUp() - will hide an element with an effect
+    // slideToggle() - will show and hide an element with an effect
+    $('.jquery').on('click','button', function(){
+        $(this).closest('.jquery').find('.jqueryFrameworks').slideDown();
+    });
 
+    // Tip: You can know how many instances did the jquery selector found with the 'length' property
+    $('p').text($('p').length);
 });
